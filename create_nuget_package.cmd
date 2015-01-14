e94120805b1d613570f932c9079201bbe5b74365
untrots <untrots@gmail.com>
2015-01-14 18:10:34 +0200

    Version: 0.1.3.0 => 0.1.4.0

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -2,30 +2,30 @@
 
 rmdir NuGetPackage /s /q
 mkdir NuGetPackage
-mkdir NuGetPackage\Epicycle.Graphics-cs.0.1.3.0
-mkdir NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib
+mkdir NuGetPackage\Epicycle.Graphics-cs.0.1.4.0
+mkdir NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib
 
-copy package.nuspec NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\Epicycle.Graphics-cs.0.1.3.0.nuspec
-copy README.md NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\README.md
-copy LICENSE NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\LICENSE
+copy package.nuspec NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\Epicycle.Graphics-cs.0.1.4.0.nuspec
+copy README.md NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\README.md
+copy LICENSE NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\LICENSE
 
-xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net35\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net35\
-xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net40\
-xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.3.0\lib\net45\
+xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net35\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net35\
+xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net40\
+xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Graphics.Platform.SystemDrawing_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Graphics_cs.dll NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Graphics_cs.pdb NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Graphics_cs.xml NuGetPackage\Epicycle.Graphics-cs.0.1.4.0\lib\net45\
 
 pause
