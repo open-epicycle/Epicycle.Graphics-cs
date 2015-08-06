d3792b3caa5397b5f116b3f07ed75ee28fc869c6
untrots <untrots@gmail.com>
2015-08-06 19:41:39 +0300

    ReferenceWhiteUtils: +GetStandardReferenceWhiteXYZ

--- projects/Epicycle.Graphics_cs/Color/Spaces/ReferenceWhiteUtils.cs
+++ projects/Epicycle.Graphics_cs/Color/Spaces/ReferenceWhiteUtils.cs
@@ -16,10 +16,14 @@
 // For more information check https://github.com/open-epicycle/Epicycle.Graphics-cs
 // ]]]]
 
+using System;
+
 namespace Epicycle.Graphics.Color.Spaces
 {
     public static class ReferenceWhiteUtils
     {
+        // Based on http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
+
         public static float[] ReferenceWhite_XYZ_A   = new float[] { 1.09850f, 1.00000f, 0.35585f };
         public static float[] ReferenceWhite_XYZ_B   = new float[] { 0.99072f, 1.00000f, 0.85223f };
         public static float[] ReferenceWhite_XYZ_C   = new float[] { 0.98074f, 1.00000f, 1.18232f };
@@ -31,5 +35,36 @@ namespace Epicycle.Graphics.Color.Spaces
         public static float[] ReferenceWhite_XYZ_F2  = new float[] { 0.99186f, 1.00000f, 0.67393f };
         public static float[] ReferenceWhite_XYZ_F7  = new float[] { 0.95041f, 1.00000f, 1.08747f };
         public static float[] ReferenceWhite_XYZ_F11 = new float[] { 1.00962f, 1.00000f, 0.64350f };
+
+        public static float[] GetStandardReferenceWhiteXYZ(StandardReferenceWhite referenceWhite)
+        {
+            switch(referenceWhite)
+            {
+                case StandardReferenceWhite.A:
+                    return ReferenceWhite_XYZ_A;
+                case StandardReferenceWhite.B:
+                    return ReferenceWhite_XYZ_B;
+                case StandardReferenceWhite.C:
+                    return ReferenceWhite_XYZ_C;
+                case StandardReferenceWhite.D50:
+                    return ReferenceWhite_XYZ_D50;
+                case StandardReferenceWhite.D55:
+                    return ReferenceWhite_XYZ_D55;
+                case StandardReferenceWhite.D65:
+                    return ReferenceWhite_XYZ_D65;
+                case StandardReferenceWhite.D75:
+                    return ReferenceWhite_XYZ_D75;
+                case StandardReferenceWhite.E:
+                    return ReferenceWhite_XYZ_E;
+                case StandardReferenceWhite.F2:
+                    return ReferenceWhite_XYZ_F2;
+                case StandardReferenceWhite.F7:
+                    return ReferenceWhite_XYZ_F7;
+                case StandardReferenceWhite.F11:
+                    return ReferenceWhite_XYZ_F11;
+                default:
+                    throw new ArgumentException("Illegal reference white");
+            }
+        }
     }
 }
